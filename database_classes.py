aba37d91520bbab0d79e7154274044b622e92649
Stefan Krsmanovic <103185975+Krsmanovic-S@users.noreply.github.com>
2025-04-28 14:05:21 +0200

    Update database_classes.py

--- database_classes.py
+++ database_classes.py
@@ -17,7 +17,9 @@ class User(UserMixin, db.Model):
     username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(50), nullable=False)
+
     posts = relationship("ForumPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
 
     def __init__(self, username, email, password):
         self.username = username
@@ -29,6 +31,7 @@ class ForumCategories(db.Model):
     __tablename__ = "forum_categories"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+
     posts = relationship("ForumPost", back_populates="category")
 
 
@@ -43,3 +46,23 @@ class ForumPost(db.Model):
 
     author = relationship("User", back_populates="posts")
     category = relationship("ForumCategories", back_populates="posts")
+    comments = relationship("Comment", back_populates="parent_post")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    post_id: Mapped[str] = mapped_column(Integer, db.ForeignKey("forum_posts.id"))
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+
+    comment_author = relationship("User", back_populates="comments")
+    parent_post = relationship("ForumPost", back_populates="comments")
+
+    parent_comment_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("comments.id"), nullable=True)
+    replies = relationship("Comment", backref=db.backref('parent_comment', remote_side='Comment.id'))
+
+    def __init__(self, comment_author, parent_post, text):
+        self.comment_author = comment_author
+        self.parent_post = parent_post
+        self.text = text
